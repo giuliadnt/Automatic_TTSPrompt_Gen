302f65a939eeac0cd41e950f042a7d95d7086b36
gdonato <gdonato@omilia.com>
2019-04-15 12:03:56 +0300

    added static methods for storing filename + texts in dicts from different file types

--- tts_generator.py
+++ tts_generator.py
@@ -7,6 +7,41 @@ import subprocess
 import argparse
 
 
+class PromptParser:
+
+	def __init__(self):
+		pass
+
+	@staticmethod
+	def create_dict_from_xml(self):
+		xml_parser = et.XMLParser(encoding='utf-8')
+		tree = et.parse(self, xml_parser)
+		xml_root = tree.getroot()
+		d = {}
+		for idx, c in enumerate(xml_root.getchildren()):
+			d[idx] = (c.get('file').replace('.wav', ''), c.get('prompt'))
+		return d
+
+	@staticmethod
+	def create_dict_from_csv(self):
+		pass
+
+	#  PERHAPS create separate class or handling for manipulation of data???
+
+	# @staticmethod
+	# def store_tts(dir_name):
+	#
+	# 	for k, v in prompts_dict.items():
+	# 		tts = gTTS(text=v[1], lang=v[2])
+	# 		tts.save(dir_name + '/' + v[0] + "_" + v[2] + ".mp3")
+	#
+	# @staticmethod
+	# def to_wav(path):
+	# 	for directory, subdirectories, files in os.walk(path):
+	# 		for f in files:
+	# 			subprocess.call(['ffmpeg', '-i', f, "${f%.*}.wav"])
+
+
 def create_new_dir(path):
 
 	current_path = os.getcwd()
@@ -22,44 +57,11 @@ def create_new_dir(path):
 	return new_path
 
 
-def create_prompts_dict(root):
-
-	d = {}
-	for idx, c in enumerate(root.getchildren()):
-		d[idx] = (c.get('file').replace('.wav', ''), c.get('prompt'))
-	return d
-
-
-def store_tts(dir_name):
-
-	for k, v in prompts_dict.items():
-		tts = gTTS(text=v[1], lang=v[2])
-		tts.save(dir_name + '/' + v[0] + "_" + v[2] + ".mp3")
-
-
-def to_wav(path):
-	for directory, subdirectories, files in os.walk(path):
-		for f in files:
-			subprocess.call(['ffmpeg', '-i', f, "${f%.*}.wav"])
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", dest="file_path", help="prompt list file", metavar="FILE")
 
 args = parser.parse_args()
 
-xml_parser = et.XMLParser(encoding='utf-8')
-
-tree = et.parse(args.file_path, xml_parser)
-
-xml_root = tree.getroot()
-
-prompts_dict = create_prompts_dict(xml_root)
-
 new_dir = create_new_dir('/tmp/%s' % args.file_path)
 
-store_tts(new_dir)
 
-to_wav(new_dir)
-
-# p1 = subprocess.Popen(['', '', ], stdout=subprocess.PIPE)
