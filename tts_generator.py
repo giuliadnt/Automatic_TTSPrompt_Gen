79437ab597a3eecfdc56957aea5730cb95eeff2b
gdonato <gdonato@omilia.com>
2019-04-15 12:04:26 +0300

    converted intents

--- tts_generator.py
+++ tts_generator.py
@@ -9,52 +9,52 @@ import argparse
 
 class PromptParser:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	@staticmethod
-	def create_dict_from_xml(self):
-		xml_parser = et.XMLParser(encoding='utf-8')
-		tree = et.parse(self, xml_parser)
-		xml_root = tree.getroot()
-		d = {}
-		for idx, c in enumerate(xml_root.getchildren()):
-			d[idx] = (c.get('file').replace('.wav', ''), c.get('prompt'))
-		return d
+    @staticmethod
+    def create_dict_from_xml(self):
+        xml_parser = et.XMLParser(encoding='utf-8')
+        tree = et.parse(self, xml_parser)
+        xml_root = tree.getroot()
+        d = {}
+        for idx, c in enumerate(xml_root.getchildren()):
+            d[idx] = (c.get('file').replace('.wav', ''), c.get('prompt'))
+        return d
 
-	@staticmethod
-	def create_dict_from_csv(self):
-		pass
+    @staticmethod
+    def create_dict_from_csv(self):
+        pass
 
-	#  PERHAPS create separate class or handling for manipulation of data???
+    #  PERHAPS create separate class or handling for manipulation of data???
 
-	# @staticmethod
-	# def store_tts(dir_name):
-	#
-	# 	for k, v in prompts_dict.items():
-	# 		tts = gTTS(text=v[1], lang=v[2])
-	# 		tts.save(dir_name + '/' + v[0] + "_" + v[2] + ".mp3")
-	#
-	# @staticmethod
-	# def to_wav(path):
-	# 	for directory, subdirectories, files in os.walk(path):
-	# 		for f in files:
-	# 			subprocess.call(['ffmpeg', '-i', f, "${f%.*}.wav"])
+    # @staticmethod
+    # def store_tts(dir_name):
+    #
+    # 	for k, v in prompts_dict.items():
+    # 		tts = gTTS(text=v[1], lang=v[2])
+    # 		tts.save(dir_name + '/' + v[0] + "_" + v[2] + ".mp3")
+    #
+    # @staticmethod
+    # def to_wav(path):
+    # 	for directory, subdirectories, files in os.walk(path):
+    # 		for f in files:
+    # 			subprocess.call(['ffmpeg', '-i', f, "${f%.*}.wav"])
 
 
 def create_new_dir(path):
 
-	current_path = os.getcwd()
-	new_path = current_path + path
+    current_path = os.getcwd()
+    new_path = current_path + path
 
-	try:  
-		os.mkdir(new_path)
-	except OSError:  
-		print("Creation of the directory %s failed or already exists" % new_path)
-	else:  
-		print("Successfully created the directory %s " % new_path)
+    try:
+        os.mkdir(new_path)
+    except OSError:
+        print("Creation of the directory %s failed or already exists" % new_path)
+    else:
+        print("Successfully created the directory %s " % new_path)
 
-	return new_path
+    return new_path
 
 
 parser = argparse.ArgumentParser()
